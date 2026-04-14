6c6c056dde61820d5a83a63b5d949cba490d0d34
Mrinmay-afk <mrinmay1009@gmail.com>
2026-04-14 22:22:09 +0530

    footer

--- FwrdClone/src/App.jsx
+++ FwrdClone/src/App.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+import Footer from './components/Footer';
+
 function App() {
-  return <h1>FWRD Clone Project</h1>;
+  return (
+    <div>
+      <h1>FWRD Clone Project</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
