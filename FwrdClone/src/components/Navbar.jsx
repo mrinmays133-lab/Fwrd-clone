e45cac4cfc272131070daf4ff283f2e65fe8cffa
sreenu <sreenivasulureddy.2025@lpu.in>
2026-04-15 16:36:46 +0530

    Modified Navbar

--- FwrdClone/src/components/Navbar.jsx
+++ FwrdClone/src/components/Navbar.jsx
@@ -1,49 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Essential for navigation
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-
       <div className="navbar-mini">
         <div className="mini-right">
-          <span>EN</span>
+          <span>🇮🇳</span>
+          <a href="#">EN</a>
           <span>|</span>
-          <span>INR</span>
-          <span>Login</span>
-          <span>Sign In</span>
+          <a href="#">INR</a>
+          <a href="#">Need Help?</a>
+          {/* Using Link for the internal Login page */}
+          <Link to="/login" className="login-link">Sign In</Link>
         </div>
       </div>
-
 
       <div className="navbar-top">
         <div className="navbar-left">
-          <span>Womens</span>
-          <span>Mens</span>
+          <a href="#">Womens</a>
+          <a href="#">Mens</a>
         </div>
 
         <div className="navbar-center">
-          <h1>FasHiOn</h1>
+          {/* Clicking the logo takes you home */}
+          <Link to="/" className="logo-link">
+            <h1>LUXION</h1>
+          </Link>
         </div>
 
         <div className="navbar-right">
-          <span>Search</span>
-          <span>My Bag (0)</span>
+          <a href="#">Search</a>
+          <a href="#">My Bag (0)</a>
         </div>
       </div>
 
-  
       <div className="navbar-bottom">
         <ul className="navbar-links">
-          <li>Clothing</li>
-          <li>Bags</li>
-          <li>Accessories</li>
-          <li>Shoes</li>
-          <li>Beauty</li>
+          <li><a href="#">Clothing</a></li>
+          <li><a href="#">Bags</a></li>
+          <li><a href="#">Accessories</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">Beauty</a></li>
         </ul>
       </div>
-
     </nav>
   );
 };
 
-export default Navbar
+export default Navbar;
