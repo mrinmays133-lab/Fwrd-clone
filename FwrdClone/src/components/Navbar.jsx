228c3bc2e73170a309b500bad03a49244316a195
Mrinmay-afk <mrinmay1009@gmail.com>
2026-04-18 00:29:19 +0530

    Login Page Updated

--- FwrdClone/src/components/Navbar.jsx
+++ FwrdClone/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
+import { auth } from "../pages/firebase"; // 
+import { onAuthStateChanged, signOut } from "firebase/auth"; 
 import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
 
 const categories = [
   { label: "CLOTHING", path: "/shop/clothing" },
@@ -15,7 +18,25 @@ const Navbar = ({ toggleTheme, theme }) => {
   const [isSearchExpanded, setIsSearchExpanded] = useState(false);
   const { cart } = useContext(CartContext);
 
+  const [user, setUser] = useState(null); 
+  const navigate = useNavigate();
+
   const itemCount = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+  // 🔥 listen for login state
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // 🔥 logout function
+  const handleLogout = async () => {
+    await signOut(auth);
+    navigate("/");
+  };
 
   return (
     <nav className="navbar">
@@ -27,24 +48,41 @@ const Navbar = ({ toggleTheme, theme }) => {
           <span>EN</span>
           <span>|</span>
           <span>INR</span>
-          <Link to="/login">Sign In</Link>
+
+          {/* 🔥 CONDITIONAL UI */}
+          {user ? (
+            <>
+              <img
+               src={
+               user.photoURL ||
+              "https://cdn-icons-png.flaticon.com/512/847/847969.png"
+              }
+              alt="profile"
+              className="profile-pic"
+              />
+
+              <button onClick={handleLogout} className="logout-btn">
+                LogOut
+              </button>
+            </>
+          ) : (
+            <Link to="/login">Sign In</Link>
+          )}
         </div>
       </div>
 
       {/* Top Nav */}
       <div className="navbar-top">
         <div className="navbar-left">
-          <span>Womens</span>
+          <span></span>
         </div>
 
-        {/* Logo */}
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
             <h1>LUXION</h1>
           </Link>
         </div>
 
-        {/* Right Section */}
         <div className="navbar-right">
           <div className="search-bar">
             <input
