b73bf0dcafbb13429ad7196c64676517159fcfee
Swalihcode <muhswalih7878@gmail.com>
2026-04-17 19:54:02 +0530

    Fix merge conflict markers in src/main.jsx

--- FwrdClone/src/components/Navbar.jsx
+++ FwrdClone/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import { CartContext } from "../context/CartContext";// Essential for navigation
+import { CartContext } from "../context/CartContext";
 import './Navbar.css';
 
 const categories = [
@@ -18,78 +18,18 @@ const Navbar = ({ toggleTheme, theme }) => {
 
   return (
     <nav className="navbar">
-      {/* Mini Nav */}
+      {/* Mini Nav - Top Bar */}
       <div className="navbar-mini">
         <div className="mini-right">
-          <span>EN</span>
+          <span>🇮🇳 EN</span>
           <span>|</span>
           <span>INR</span>
-          <span><Link to="/login">Sign In</Link></span>
-        </div>
-      </div>
-
-      {/* Top Nav */}
-      <div className="navbar-top">
-        <div className="navbar-left">
-         
-        </div>
-
-        <div className="navbar-center">
-          <h1>
-            <Link to="/">LUXION</Link>
-          </h1>
-        </div>
-      <div className="navbar-mini">
-        <div className="mini-right">
-          <span>🇮🇳</span>
-          <a href="#">EN</a>
-          <span>|</span>
-          <a href="#">INR</a>
-          <a href="#">Need Help?</a>
-          {/* Using Link for the internal Login page */}
           <Link to="/login" className="login-link">Sign In</Link>
-          <span>INR</span>
-          <span><Link to="/login">Sign In</Link></span>
-          {/* Replace <a> with <Link> and point to the "/login" path */}
-          <Link to="/login" className="login-link">Sign In</Link> {/* */}
         </div>
       </div>
 
+      {/* Top Nav - Logo & Search */}
       <div className="navbar-top">
-        <div className="navbar-left">
-          <a href="#">Womens</a>
-          <a href="#">Mens</a>
-        </div>
-
-        <div className="navbar-center">
-          {/* Clicking the logo takes you home */}
-          <Link to="/" className="logo-link">
-            <h1>LUXION</h1>
-          </Link>
-        </div>
-
-        <div className="navbar-right">
-          <a href="#">Search</a>
-          <a href="#">My Bag (0)</a>
-          <span>Womens</span>
-        </div>
-
-        <div className="navbar-center">
-
-          <h1>
-            <Link to="/">LUXION</Link>
-          </h1>
-
-          {/* Link the logo back to the home page */}
-          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <h1>FasHiOn</h1>
-          </Link> {/* */}
-      {/* Top */}
-      <div className="navbar-top">
-        <div className="navbar-spacer"></div>
-
-
-        {/* Logo */}
         <div className="navbar-logo">
           <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
             LUXION
@@ -106,44 +46,12 @@ const Navbar = ({ toggleTheme, theme }) => {
               onBlur={() => setIsSearchExpanded(false)}
             />
           </div>
-          <Link to="/cart">My Bag ({itemCount})</Link>
-        </div>
-      </div>
-
-      <div className="navbar-bottom">
-        <ul className="navbar-links">
-          <li><a href="#">Clothing</a></li>
-          <li><a href="#">Bags</a></li>
-          <li><a href="#">Accessories</a></li>
-          <li><a href="#">Shoes</a></li>
-          <li><a href="#">Beauty</a></li>
-          <li><Link to="/shop">Clothing</Link></li>
-          <li><Link to="/shop">Bags</Link></li>
-          <li><Link to="/shop">Accessories</Link></li>
-          <li><Link to="/shop">Shoes</Link></li>
-          <li><Link to="/shop">Beauty</Link></li>
-
-        </ul>
-        {/* Actions */}
-        <div className="navbar-actions">
-          <Link to="/login" className="navbar-link">
-            Sign In
-          </Link>
-
-        <div className="navbar-right">
-          <div className="search-bar">
-            <input
-              type="text"
-              placeholder="Search"
-              className={`search-input ${isSearchExpanded ? 'expanded' : ''}`}
-              onFocus={() => setIsSearchExpanded(true)}
-              onBlur={() => setIsSearchExpanded(false)}
-            />
-          </div>
           <button onClick={toggleTheme} className="theme-toggle" title="Toggle dark mode">
             {theme === 'light' ? '🌙' : '☀️'}
           </button>
-          <Link to="/cart">My Bag ({itemCount})</Link>
+          <Link to="/cart" className="cart-link">
+            My Bag ({itemCount})
+          </Link>
         </div>
       </div>
 
@@ -151,7 +59,9 @@ const Navbar = ({ toggleTheme, theme }) => {
       <div className="navbar-bottom">
         <ul className="navbar-links">
           {categories.map((cat) => (
-            <li key={cat.label}><Link to={cat.path}>{cat.label}</Link></li>
+            <li key={cat.label}>
+              <Link to={cat.path}>{cat.label}</Link>
+            </li>
           ))}
         </ul>
       </div>
